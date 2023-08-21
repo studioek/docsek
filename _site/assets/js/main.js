7146b4a58e640542d34295945c33dd64ee4b101d
Emre Kayık <54414075+emrekayik@users.noreply.github.com>
2023-08-21 20:58:48 +0300

    Delete _site directory

--- _site/assets/js/main.js
+++
@@ -1,9 +0,0 @@
-// Alert message
-document.addEventListener("DOMContentLoaded", function () {
-    const alert = document.querySelector(".alert");
-    if (alert) {
-        setTimeout(function () {
-            alert.classList.add("hidden");
-        }, 3000);
-    }
-});
